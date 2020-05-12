b2157317d4623884d12fcfb3ad5ab5478fd39824
Gerald Rieß <g.riess@gmx.at>
2020-05-12 21:41:13 +0200

    create mockdata

--- sql/fh_2020_scm4_s1810307037.sql
+++ sql/fh_2020_scm4_s1810307037.sql
@@ -89,6 +89,7 @@ CREATE TABLE `user` (
   `deletedFlag` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 --
 -- Indizes der exportierten Tabellen
 --
@@ -161,6 +162,43 @@ ALTER TABLE `shoppinglist`
   ADD CONSTRAINT `shoppingList_user_FK1` FOREIGN KEY (`ownerId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `shoppingList_user_FK2` FOREIGN KEY (`helperId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+--
+-- MOCKDATA user table
+--
+
+insert into user (id, firstName, lastName, userName, password, deletedFlag) VALUES (1, 'Gerald', 'Riess', 'Hilfesuchender', '2216d6cdd869e0cbdd4a51f74e17e1ba7150db5f', false);
+insert into user (id, firstName, lastName, userName, password, deletedFlag) VALUES (2, 'Manuel', 'Strasser', 'Freiwilliger', '4b35ad500817192730ce10a1335b469372cd92fd', false);
+
+--
+-- MOCKDATA role table
+--
+insert into role (id, name, bitCode, deletedFlag) values (1, 'admin', 1111111, false);
+insert into role (id, name, bitCode, deletedFlag) values (2, 'helpSeeker', 0000001, false);
+insert into role (id, name, bitCode, deletedFlag) values (3, 'helper', 0000002, false);
+
+--
+-- MOCKDATA shopping list table
+--
+insert into shoppinglist (id, ownerId, helperId, endDate, paidPrice, state, name) values (1, 1, 2, '2/3/2020', 46.55, 'not published', 'Cookley');
+insert into shoppinglist (id, ownerId, helperId, endDate, paidPrice, state, name) values (2, 1, 2, '1/3/2020', 38.67, 'new', 'Sub-Ex');
+insert into shoppinglist (id, ownerId, helperId, endDate, paidPrice, state, name) values (3, 1, 2, '2/13/2020', 23.87, 'processing', 'Stringtough');
+insert into shoppinglist (id, ownerId, helperId, endDate, paidPrice, state, name) values (4, 1, 2, '1/29/2020', 55.35, 'done', 'Job');
+
+--
+-- MOCKDATA article table
+--
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (1, 1, 'Nut - Pumpkin Seeds', 1, 33.3, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (2, 1, 'Pork - Tenderloin, Frozen', 2, 18.2, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (3, 1, 'Shrimp - 16 - 20 Cooked, Peeled', 3, 3.4, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (4, 1, 'Yogurt - French Vanilla', 4, 35.5, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (5, 2, 'Wine - White, Chardonnay', 5, 33.52, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (6, 2, 'Cranberries - Fresh', 6, 24.23, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (7, 2, 'Potatoes - Fingerling 4 Oz', 7, 89.12, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (8, 2, 'Pepper - Sorrano', 8, 97.65, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (9, 3, 'Scampi Tail', 9, 57.11, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (10, 3, 'Coriander - Seed', 10, 89.23, false, false);
+insert into article (id, shoppingListId, description, amount, highestPrice, deletedFlag, doneFlag) VALUES (11, 3, 'Cheese - Parmesan Cubes', 11, 38.78, false, false);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
